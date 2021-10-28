6ecea2215053ea60f6feeb72613940ff2570f213
Sverrir Sigfusson <Sveppi@Sverrirs-MacBook-Pro.local>
2021-10-28 20:00:29 +0000

    uppfærsla

--- scripts.js
+++ scripts.js
@@ -18,40 +18,33 @@ let losses = 0;
  * @return {boolean} true eða false
  */
 function isValidBestOf(bestOf) {
-  if (bestOf === null){
-    console.error("Spilari hætti við")
+  if (bestOf === null) {
+    console.error("Spilari hætti við");
     process.exit(0);
-  }
-  else if (bestOf >= MAX_BEST_OF || bestOf < 1){
+  } else if (bestOf >= MAX_BEST_OF || bestOf < 1) {
     console.error("Fjöldi leikja utan marka");
     process.exit(0);
-  }
-  else{
-    if (bestOf % 2 === 0){
-      alert(bestOf + " er ekki gildur best of");
-      return false;
-    }
-    else{
+  } else {
+    if (bestOf % 2 === 0) {
+      console.error(bestOf + " er ekki gildur best of");
+      process.exit(0);
+    } else {
       return true;
     }
-    }
   }
+}
 
 function playAsText(play) {
   // TODO útfæra
-  if (play === "1"){
+  if (play === "1") {
     return "skæri";
-  }
-  else if (play === "2"){
+  } else if (play === "2") {
     return "steinn";
-  }
-  else if (play === "3"){
+  } else if (play === "3") {
     return "blað";
-  }
-  else if (play === null){
-   process.exit(0);
-  }
-  else{
+  } else if (play === null) {
+    process.exit(0);
+  } else {
     return "Óþekkt";
   }
 }
@@ -64,22 +57,25 @@ function playAsText(play) {
  */
 function checkGame(player, computer) {
   // TODO útfæra
-  if(player === '1' && computer === '2' || 
-      player === '2' && computer === '3' ||
-      player === '3' && computer === '1'){
-      return 1;
-    }
-  else if(player === '2' && computer === '1' || 
-      player === '3' && computer === '2' ||
-      player === '1' && computer === '3'){
-        return -1;
-    }
-  else if (player === '2' && computer === '2' || 
-      player === '3' && computer === '3' ||
-      player === '1' && computer === '1'){
+  if (
+    (player === "1" && computer === "3") ||
+    (player === "2" && computer === "1") ||
+    (player === "3" && computer === "2")
+  ) {
+    return 1;
+  } else if (
+    (player === "1" && computer === "2") ||
+    (player === "2" && computer === "3") ||
+    (player === "3" && computer === "1")
+  ) {
+    return -1;
+  } else if (
+    (player === "1" && computer === "1") ||
+    (player === "2" && computer === "2") ||
+    (player === "3" && computer === "3")
+  ) {
     return 0;
   }
- 
 }
 
 /**
@@ -88,32 +84,42 @@ function checkGame(player, computer) {
  */
 function round() {
   // 1. Spyrja um hvað spilað, ef cancel, hætta
-  const player = prompt("Skæri (1), steinn (2), blað(3)?");
-  console.log("input:" + player);
-  
-  if (player == null){
+  const player = prompt("Sláðu inn tölu: Skæri (1), steinn (2), blað (3)?");
+ 
+  // 2. Ef ógilt, tölva vinnur
+  if (player == null) {
     process.exit(0);
-  }
-  else if (playAsText(player) === "Óþekkt"){
+  } else if (playAsText(player) === "Óþekkt") {
+    alert("Ólöglegt inntak, tölvan vann þessa lotu :(")
     return -1;
   }
-  
-  // 2. Ef ógilt, tölva vinnur
+
+ 
   // 3. Velja gildi fyrir tölvu með `Math.floor(Math.random() * 3) + 1` sem skilar heiltölu á [1, 3]
   const computer = (Math.floor(Math.random() * 3) + 1).toString();
+  
   // 4. Nota `checkGame()` til að finna hver vann
   // 5. Birta hver vann
-
   let roundResult = checkGame(player, computer);
 
-  if (roundResult === 1){
-    confirm(`Spilari valdi ${playAsText(player)} og tölvan valdi ${playAsText(computer)}. Spilari vann!`)
-  }
-  else if (roundResult === -1){
-    confirm(`Spilari valdi ${playAsText(player)} og tölvan valdi ${playAsText(computer)}. Tölvan vann :(`);
-  }
-  else if(roundResult === 0){
-    confirm(`Spilari valdi ${playAsText(player)} og tölvan valdi ${playAsText(computer)}. Jafntefli!`);
+  if (roundResult === 1) {
+    confirm(
+      `Spilari valdi ${playAsText(player)} og tölvan valdi ${playAsText(
+        computer
+      )}. Spilari vann lotuna!`
+    );
+  } else if (roundResult === -1) {
+    confirm(
+      `Spilari valdi ${playAsText(player)} og tölvan valdi ${playAsText(
+        computer
+      )}. Tölvan vann lotuna :(`
+    );
+  } else if (roundResult === 0) {
+    confirm(
+      `Spilari valdi ${playAsText(player)} og tölvan valdi ${playAsText(
+        computer
+      )}. Jafntefli!`
+    );
   }
   // 6. Skila hver vann
   return roundResult;
@@ -123,51 +129,55 @@ function round() {
  * Spilar leik og bætir útkomu (sigur eða tap) við í viðeigandi global breytu.
  */
 function play() {
-
   let playerRounds = 0;
   let computerRounds = 0;
-  
+
   // 1. Spyrja um fjölda leikja
-  const bestOf = prompt("Besta af?");
-  
+  const bestOf = prompt("Hvað viltu spila upp í best-of?");
+
   // 2. Staðfesta að fjöldi leikja sé gilt gildi
   // 3. Keyra fjölda leikja og spila umferð þar til sigurvegari er krýndur
-  if (isValidBestOf(bestOf)){
-    while (playerRounds < Math.floor((bestOf/2+1)) && computerRounds < Math.floor((bestOf/2+1))){
+  if (isValidBestOf(bestOf)) {
+    while (
+      playerRounds < Math.floor(bestOf / 2 + 1) &&
+      computerRounds < Math.floor(bestOf / 2 + 1)
+    ) {
       const result = round();
-      if (result === 1){ 
+      if (result === 1) {
         playerRounds++;
-      }
-      else if (result === -1){ 
+      } else if (result === -1) {
         computerRounds++;
       }
-      console.log("spilar:" + playerRounds);
-      console.log("computer:" + computerRounds);
+      console.log("Stig spilara:" + playerRounds);
+      console.log("Stig tölvu:" + computerRounds);
     }
   }
-  
+
   // 4. Birta hvort spilari eða tölva vann
-  if(playerRounds >= Math.floor((bestOf/2+1))){
+  if (playerRounds >= Math.floor(bestOf / 2 + 1)) {
     confirm("Spilari vann leikinn!");
     wins++;
-  }
-  else{
+  } else {
     confirm("Tölvan vann leikinn :(");
-    losses++
+    losses++;
   }
-  
+
   playerRounds = 0;
   computerRounds = 0;
 }
-// Hér getum við ekki skrifað test þar sem fallið mun biðja notanda um inntak!
 
 /**
  * Birtir stöðu spilara.
  */
 function games() {
-  if ((wins+losses) > 0){
-  confirm(`Þú hefur spilað ${wins + losses} leiki.\n Þú hefur unnið ${wins}, eða ${100*(wins/(wins+losses)).toFixed(2)}% af heild.`);
-}
-else confirm("Þú hefur ekki spilað leik");
+  if (wins + losses > 0) {
+    confirm(`Þú hefur spilað ${wins + losses} leiki.\n
+          Þú hefur unnið ${wins}, eða ${
+      100 * (wins / (wins + losses)).toFixed(2)
+    }% af heild.\n
+          Þú hefur tapað ${loses}, eða ${
+      100 * (losses / (wins + losses)).toFixed(2)
+    }% af heild.`);
+  } else confirm("Þú hefur spilað 0 leiki");
 }
 // Hér getum við ekki skrifað test þar sem fallið les úr global state
